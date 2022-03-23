6eefa9684f446b215e17816dc3ec39e0975e3d3f
Rotem Meiri <rotemx@users.noreply.github.com>
2022-03-23 20:54:26 +0200

    added jQuery reusable component

--- js/script.js
+++ js/script.js
@@ -2,35 +2,56 @@
 // authorization - is Rotem allowed access to the resource? and what level of access?
 
 let
-	result_list,
-	search_term_input,
-	type_select_el;
+	$result_list,
+	$search_term_input,
+	$type_select_el;
 
 function onBodyLoad()
 {
-	result_list       = document.getElementById('result-list');
-	search_term_input = document.getElementById('search-box');
-	type_select_el    = document.getElementById('type-select');
+	$result_list       = $('#result-list');
+	$search_term_input = $('#search-box');
+	$type_select_el    = $('#type-select');
 }
 
 
-function createItemHtmlElement(item){
-	const
-		textNode = document.createTextNode(item.trackName),
-		div      = document.createElement('div');
-	div.appendChild(textNode);
-	return div;
+function createItemHtmlElement(item) //encapsulation
+{
+	switch(item.kind) {
+	
+	}
+	
+	// Reusable component
+	const template = `
+		<div class="song-item-wrapper">
+			<img src="${item.artworkUrl60}" height="60">
+			<div class="song-fields">
+				<div class="song-title">
+					${item.trackName}
+				</div>
+				<div class="album-title">
+					${item.collectionName}
+				</div>
+				<div class="artist-title">
+					${item.artistName}
+				</div>
+			</div>
+			<div class="song-player">
+				<audio src="${item.previewUrl}" controls></audio>"
+			</div>
+		</div>
+	`
+	return $(template);
 }
 
 
 function onButtonClicked()
 {
-	result_list.innerHTML = '';
+	$result_list.innerHTML = '';
 	const
-		search_term       = search_term_input.value,
-		searchType        = type_select_el.value;
+		search_term        = $search_term_input.val(),
+		searchType         = $type_select_el.val();
 	
-	fetch(`https://itunes.apple.com/search?term=${search_term}${searchType === 'all' ?  '' : '&entity=' + searchType }`)
+	fetch(`https://itunes.apple.com/search?term=${search_term}${searchType === 'all' ? '' : '&entity=' + searchType}`)
 		.then(response => {
 			response
 				.json()
@@ -38,12 +59,10 @@ function onButtonClicked()
 					const items = data
 						.results
 						.filter(item => !!item.kind);
-					
-					
 					for (let item of items)
 					{
 						const element = createItemHtmlElement(item);
-						result_list.appendChild(element);
+						$result_list.append(element);
 					}
 				})
 		})
