1f47e5378885d55bf4343457269f31bc20c1eaf0
Rotem Meiri <rotemx@users.noreply.github.com>
2022-03-23 20:13:32 +0200

    extracted div creation into a function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,51 @@
+// authentication - is it really Rotem ?
+// authorization - is Rotem allowed access to the resource? and what level of access?
+
 let
-    result_list,
-    search_term_input;
+	result_list,
+	search_term_input,
+	type_select_el;
 
-function onBodyLoad(){
-    result_list = document.getElementById('result-list');
-    search_term_input = document.getElementById('search-box');
+function onBodyLoad()
+{
+	result_list       = document.getElementById('result-list');
+	search_term_input = document.getElementById('search-box');
+	type_select_el    = document.getElementById('type-select');
 }
 
-function onButtonClicked() {
-    result_list.innerHTML = '';
-    const search_term = search_term_input.value;
-    fetch(`https://itunes.apple.com/search?term=${search_term}`)
-        .then(response => {
-            response
-                .json()
-                .then(data => {
-                    const albums = data
-                        .results
-                        .map(res => res.collectionName);
 
-                    const unique_album = [...(new Set(albums))];
-
-                    for (let album of unique_album) {
-                        const
-                            text = document.createTextNode(album),
-                            div = document.createElement('div');
-                        div.appendChild(text);
-                        result_list.appendChild(div);
-                    }
-                })
-        })
+function createItemHtmlElement(item){
+	const
+		textNode = document.createTextNode(item.trackName),
+		div      = document.createElement('div');
+	div.appendChild(textNode);
+	return div;
 }
+
+
+function onButtonClicked()
+{
+	result_list.innerHTML = '';
+	const
+		search_term       = search_term_input.value,
+		searchType        = type_select_el.value;
+	
+	fetch(`https://itunes.apple.com/search?term=${search_term}${searchType === 'all' ?  '' : '&entity=' + searchType }`)
+		.then(response => {
+			response
+				.json()
+				.then(data => {
+					const items = data
+						.results
+						.filter(item => !!item.kind);
+					
+					
+					for (let item of items)
+					{
+						const element = createItemHtmlElement(item);
+						result_list.appendChild(element);
+					}
+				})
+		})
+}
+
