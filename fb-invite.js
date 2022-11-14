8be695fe5689fc601e11963fbbacecb460e2171b
Rotem Meiri <rotemx@users.noreply.github.com>
2022-11-14 17:44:17 +0200

    fixed fb invite

--- fb-invite.js
+++ fb-invite.js
@@ -8,16 +8,16 @@ function loadJquery()
 		} : t(e)
 	}("undefined" != typeof window ? window : this, function (C, e) {
 		"use strict";
-		var t                                                                                             = [], r = Object.getPrototypeOf, s = t.slice, g = t.flat ? function (e) {
+		var t                                                                                             = [], r                                                                                     = Object.getPrototypeOf, s = t.slice, g = t.flat ? function (e) {
 			    return t.flat.call(e)
 		    } : function (e) {
 			    return t.concat.apply([], e)
 		    }, u = t.push, i = t.indexOf, n = {}, o = n.toString, y = n.hasOwnProperty, a = y.toString, l = a.call(Object),
-		    v                                                                                             = {}, m                                                                                     = function (e) {
+		    v                                                                                             = {}, m = function (e) {
 			    return "function" == typeof e && "number" != typeof e.nodeType && "function" != typeof e.item
 		    }, x                                                                                          = function (e) {
 			    return null != e && e === e.window
-		    }, E                                                                                          = C.document, c = {type: !0, src: !0, nonce: !0, noModule: !0};
+		    }, E                                                                                          = C.document, c                                                                          = {type: !0, src: !0, nonce: !0, noModule: !0};
 		
 		function b(e, t, n)
 		{
@@ -120,8 +120,8 @@ function loadJquery()
 		});
 		var d = function (n) {
 			var e, d, b, o, i, h, f, g, w, u, l, T, C, a, E, y, s, c, v, S = "sizzle" + 1 * new Date, p = n.document,
-			    k                                                                                       = 0, r = 0, m                                                                         = ue(), x                                                               = ue(), A                                                     = ue(),
-			    N                                                                                       = ue(), j                                                                             = function (e, t) {
+			    k                                                                                       = 0, r = 0, m = ue(), x                                                               = ue(), A = ue(),
+			    N                                                                                       = ue(), j = function (e, t) {
 				    return e === t && (l = !0), 0
 			    }, D                                                                                    = {}.hasOwnProperty, t                                                             = [], q = t.pop,
 			    L                                                                                       = t.push, H = t.push, O = t.slice,
@@ -150,7 +150,7 @@ function loadJquery()
 				    needsContext: new RegExp("^" + M + "*[>+~]|:(even|odd|eq|gt|lt|nth|first|last)(?:\\(" + M + "*((?:-\\d)?\\d*)" + M + "*\\)|)(?=[^-]|$)", "i")
 			    }, Y                                                                                    = /HTML$/i,
 			    Q                                                                                       = /^(?:input|select|textarea|button)$/i,
-			    J                                                                                       = /^h\d$/i, K = /^[^{]+\{\s*\[native \w/,
+			    J                                                                                       = /^h\d$/i, K                                                                         = /^[^{]+\{\s*\[native \w/,
 			    Z                                                                                       = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/,
 			    ee                                                                                      = /[+~]/,
 			    te                                                                                      = new RegExp("\\\\[\\da-fA-F]{1,6}" + M + "?|\\\\([^\\r\\n\\f])", "g"),
@@ -477,7 +477,7 @@ function loadJquery()
 							return !!e.parentNode
 						} : function (e, t, n) {
 							var r, i, o, a, s, u, l = v !== m ? "nextSibling" : "previousSibling", c = e.parentNode,
-							    f                                                                    = x && e.nodeName.toLowerCase(), p                                 = !n && !x, d                   = !1;
+							    f                                                                    = x && e.nodeName.toLowerCase(), p                                 = !n && !x, d = !1;
 							if (c)
 							{
 								if (v)
@@ -644,7 +644,7 @@ function loadJquery()
 			function Ce(d, h, g, y, v, e)
 			{
 				return y && !y[S] && (y = Ce(y)), v && !v[S] && (v = Ce(v, e)), le(function (e, t, n, r) {
-					var i, o, a, s = [], u = [], l = t.length, c = e || function (e, t, n) {
+					var i, o, a, s                               = [], u = [], l = t.length, c = e || function (e, t, n) {
 						    for (var r = 0, i = t.length; r < i; r++) se(e, t[r], n);
 						    return n
 					    }(h || "*", n.nodeType ? [n] : n, []), f = !d || !e && h ? c : Te(c, s, d, n, r),
@@ -1874,7 +1874,7 @@ function loadJquery()
 						return Ze(e, u, n)
 					})
 				}, set: function (e, t, n) {
-					var r, i                                                             = Me(e), o                                                  = !v.scrollboxSize() && "absolute" === i.position,
+					var r, i                                                             = Me(e), o = !v.scrollboxSize() && "absolute" === i.position,
 					    a = (o || n) && "border-box" === S.css(e, "boxSizing", !1, i), s = n ? Ke(e, u, n, a, i) : 0;
 					return a && o && (s -= Math.ceil(e["offset" + u[0].toUpperCase() + u.slice(1)] - parseFloat(i[u]) - Ke(e, u, "border", !1, i) - .5)), s && (r = te.exec(t)) && "px" !== (r[3] || "px") && (e.style[u] = t, t = S.css(e, u)), Je(0, t, s)
 				}
@@ -2261,7 +2261,7 @@ function loadJquery()
 					                         }
 				                         }, select: {
 					                         get   : function (e) {
-						                         var t, n, r, i                     = e.options, o      = e.selectedIndex,
+						                         var t, n, r, i                     = e.options, o = e.selectedIndex,
 						                             a = "select-one" === e.type, s = a ? null : [],
 						                             u                              = a ? o + 1 : i.length;
 						                         for (r = o < 0 ? u : a ? o : 0; r < u; r++) if (((n = i[r]).selected || r === o) && !n.disabled && (!n.parentNode.disabled || !A(n.parentNode, "optgroup")))
@@ -2386,7 +2386,7 @@ function loadJquery()
 			}
 		               });
 		var Dt = /%20/g, qt = /#.*$/, Lt = /([?&])_=[^&]*/, Ht = /^(.*?):[ \t]*([^\r\n]*)$/gm, Ot = /^(?:GET|HEAD)$/,
-		    Pt                                                                                    = /^\/\//, Rt                                                                      = {}, Mt = {}, It = "*/".concat("*"), Wt = E.createElement("a");
+		    Pt                                                                                    = /^\/\//, Rt                                                                      = {}, Mt = {}, It                                                    = "*/".concat("*"), Wt = E.createElement("a");
 		
 		function Ft(o)
 		{
@@ -2904,8 +2904,8 @@ for (let i = 0; i < NUMBER_OF_OPERATIONS; i++)
 	if (list[0])
 	{
 		console.log('LIST FOUND');
-	}
-	else {
+	} else
+	{
 		console.warn('OOPS...LIST NOT FOUND');
 		
 		continue
@@ -2926,25 +2926,25 @@ for (let i = 0; i < NUMBER_OF_OPERATIONS; i++)
 		selectNextBtn.click();
 		
 		const
-			leftSpan               = $('div:contains("INVITES LEFT")').last(),
-			isSelectionComplete =  leftSpan.text() === "0 INVITES LEFT";
+			leftSpan            = $('div:contains("INVITES LEFT")').last(),
+			isSelectionComplete = leftSpan.text() === "0 INVITES LEFT";
 		
 		
 		if (isSelectionComplete)
 		{
 			console.log(isSelectionComplete, `isSelectionComplete. BREAKING at page ${i} `);
 			break;
-		// }
-		await wait(.1);
+		}
+			await wait(.1);
+			
+		}
+		await wait(5);
 		
+		console.log('SENDING INVITES...');
+		sendInvitesBtn.click();
+		console.log(opId + ' time');
+		console.timeEnd(opId)
+		
+		await wait(8);
 	}
-	await wait(5);
-	
-	console.log('SENDING INVITES...');
-	sendInvitesBtn.click();
-	console.log(opId + ' time');
-	console.timeEnd(opId)
-	
-	await wait(8);
-}
 
